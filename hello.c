a298c6ea3e17bdcc7a43c386dedf8eae09935ef2
Benjamin Chaambwa <benjaminchaambwa1@gmail.com>
2024-12-10 22:48:58 +0200

    hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 int main(void)
 {
-   printf("Hello, World\n");
-   printf("This line was added from the terminal");
-   printf("I now know Git and able to use it for my collaboration and projects");
+   printf("I updated the code because the previous code was useless");
 }
