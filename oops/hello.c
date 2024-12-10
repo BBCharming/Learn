d6aa0ec32da2169b93379c09b198b11640756d3f
Benjamin Chaambwa <benjaminchaambwa1@gmail.com>
2024-12-10 11:04:49 +0200

    oops

---
+++ oops/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+   printf("Hello, World\n");
+   printf("This line was added from the terminal");
+   printf("I now know Git and able to use it for my collaboration and projects");
+}
